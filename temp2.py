c11f4e6fefc4bda42851da46ad801d33c85db957
Tamago254 <mapdm3286@gmail.com>
2019-08-06 02:54:42 +0900

    temp

--- temp2.py
+++ temp2.py
@@ -8,7 +8,7 @@ Spyderエディタ
 from selenium import webdriver	
 
 # chromedriverのPATHを指定(herokuにおけるパスを指定しています)
-    driver_path = '/app/.chromedriver/bin/chromedriver'
+    driver_path = '/app/.apt/usr/bin/google-chrome'
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     #driverに設定 ※optionsを指定しないとheadlessにならないので注意
