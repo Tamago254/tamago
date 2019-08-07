d8bc5ed9e2d1a93fff330c0d76235474690d0f4b
Tamago254 <mapdm3286@gmail.com>
2019-08-07 20:46:44 +0900

    temp

---
+++ temp2/temp2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+
+from selenium import webdriver	
+
+# chromedriverのPATHを指定(herokuにおけるパスを指定しています)
+    driver_path = '/app/.apt/usr/bin/google-chrome'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    #driverに設定 ※optionsを指定しないとheadlessにならないので注意
+    driver = webdriver.Chrome(options=options, executable_path=driver_path)
+    driver.get("https://freebitco.in/?op=home")
+
